abe2286a86ac73e30d448c6210f9b2172d96d9fb
Nwachi <kennwachukz@gmail.com>
2024-04-23 14:26:49 +0200

    Exercise3_1&2

---
+++ Exercise3_1.py
@@ -0,0 +1,10 @@
+letter = "c"
+
+a = 'this is a vowel' # a,e,i,o,u
+b = 'this is not a vowel' 
+# b,c,d,f,g,h,j,k,l,m,n,p,q,r,s,t,v,w,x,y,z
+
+if letter == "e" or letter == "a" or letter == "u" or letter == "i" or letter == "o" :
+    print(a)
+else:
+    print(b)
---
+++ Exercise3_2.py
@@ -0,0 +1,9 @@
+number = 1420
+if number < 1000 or number > 2000:
+    if number < 1000:
+        print("number < 1000")
+
+    else:
+        print("number > 2000")
+else:
+    print("This number is in between 1000 and 2000")
