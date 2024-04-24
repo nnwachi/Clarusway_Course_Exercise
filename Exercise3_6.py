b5d364a024d091af9b1ee18dcdb40936c952b9a4
Nwachi <kennwachukz@gmail.com>
2024-04-25 00:18:21 +0200

    Exercise3_7

--- Exercise3_6.py
+++ Exercise3_6.py
@@ -3,5 +3,5 @@ if string * 6:
     print(string)
     string = 'jacob'[::-1]
     print(string)
-    def (reversewordstring (string))
-    print(string)
+string / t:
+print(string)
---
+++ Exercise3_7.py
@@ -0,0 +1,14 @@
+a = 2
+b = 2
+c = a * b
+print(c)
+
+d = 6
+e = 3
+f = d / e
+print(f)
+
+g = 9
+h = 9
+i = g * h
+print(i)
